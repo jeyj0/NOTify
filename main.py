c92e3e285b64e2de53426df9cb39afeb743fb9f8
Jannis Jorre <jeyj0.plus@gmail.com>
2018-11-09 08:59:59 +0100

    Reading data from data-file

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("test")
+import json
+from collections import namedtuple
+
+filepath = "./data/-00_announcements/2018-10-15.json"
+
+
+def loadFromJSONString(data):
+    return json.loads(data, object_hook=lambda d: namedtuple(
+        'X', d.keys())(*d.values()))
+
+
+with open(filepath, 'r') as myfile:
+    jsonString = myfile.read().replace('\n', '')
+    data = loadFromJSONString(jsonString)
+    print(data)
