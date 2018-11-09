79825d9f2420bfe8baef8c41f1299d27bdd0669c
Jannis Jorre <jeyj0.plus@gmail.com>
2018-11-09 09:17:02 +0100

    Get all messages mapped to channel; filter empty messages

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import json
 from collections import namedtuple
+from os import listdir
 
 filepath = "./data/-00_announcements/2018-10-15.json"
+datapath = "./data"
 
 
 def loadFromJSONString(data):
@@ -9,7 +11,35 @@ def loadFromJSONString(data):
         'X', d.keys())(*d.values()))
 
 
-with open(filepath, 'r') as myfile:
-    jsonString = myfile.read().replace('\n', '')
-    data = loadFromJSONString(jsonString)
-    print(data)
+def getChannels(path):
+    return listdir(path)
+
+
+def getFiles(path):
+    return listdir(path)
+
+
+messages = []
+
+
+def meaningfulMessage(text):
+    return text != ''
+
+
+def processFile(filepath, channel):
+    with open(filepath, 'r') as myfile:
+        jsonString = myfile.read().replace('\n', '')
+        data = loadFromJSONString(jsonString)
+        for msg in data:
+            if not hasattr(msg, 'subtype'):  # and not msg.subtype in subtypes:
+                if meaningfulMessage(msg.text):
+                    messages.append((channel, msg.text))
+
+
+for c in getChannels(datapath):
+    channelPath = datapath + "/" + c
+    for f in getFiles(channelPath):
+        filepath = channelPath + "/" + f
+        processFile(filepath, c)
+
+print(messages)
