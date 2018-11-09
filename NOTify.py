675925cc831edb813b4c21cb6566a1159d32290c
Jannis Jorre <jeyj0.plus@gmail.com>
2018-11-09 11:04:43 +0100

    Make NOTify actually filter messages

--- NOTify.py
+++ NOTify.py
@@ -5,4 +5,16 @@ classifier = FastText.load_model('slack_model.bin')
 text = input('Please enter a notification-text: ')
 
 output = classifier.predict([text])[0][0][0]
-print('I would say this notification comes from the channel: ' + output)
+
+channelToWorkMap = {
+    '__label__-00_announcements': True,
+    '__label__-01_general': True,
+    '__label__-07_community': False,
+    '__label__-12_random-fun': False,
+    '__label__unrestricted-chat': False
+}
+
+if channelToWorkMap[output]:
+    print('New message in ' + output)
+else:
+    print('(silence)')
